d9ff7c6b14d5b19d2e17b661706b4a710f44d39f
KingaStefanska <kinga0stefanska@gmail.com>
2023-01-24 17:54:42 +0100

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -6,33 +6,33 @@ export const API_KEY = '39ea42ad0e0931902f179ffc59d3f46a';
 
 export const getTrending = async () => {
   const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
-  return response.data;
+  return response.data.results;
 };
 
 export const getQuery = async query => {
   const response = await axios.get(
     `search/movie?api_key=${API_KEY}&query=${query}`
   );
-  return response.data;
+  return response.data.results;
 };
 
 export const getDetails = async movieId => {
   const response = await axios.get(`movie/${movieId}?api_key=${API_KEY}`);
-  return response.data;
+  return response.data.results;
 };
 
 export const getCast = async movieId => {
   const response = await axios.get(
     `movie/${movieId}/credits?api_key=${API_KEY}`
   );
-  return response.data.cast;
+  return response.data.results;
 };
 
 export const getReviews = async movieId => {
   const response = await axios.get(
     `movie/${movieId}/reviews?api_key=${API_KEY}`
   );
-  return response.data;
+  return response.data.results;
 };
 
 const api = {
