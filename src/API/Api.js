e4be22a7600e6798040b87145be4d0dd95cf8128
KingaStefanska <kinga0stefanska@gmail.com>
2023-01-25 09:50:40 +0100

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -18,7 +18,7 @@ export const getQuery = async query => {
 
 export const getDetails = async movieId => {
   const response = await axios.get(`movie/${movieId}?api_key=${API_KEY}`);
-  return response.data;
+  return response.data.results;
 };
 
 export const getCast = async movieId => {
